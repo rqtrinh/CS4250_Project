ef3018249cac23d7d90a1252c75a027e3c8621d3
mikephu <mikephu03@gmail.com>
2023-12-10 16:36:27 -0800

    add core functionality

--- ranker.py
+++ ranker.py
@@ -1,54 +1,74 @@
 from pymongo import MongoClient
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import string
-import pandas as pd
-
-# Resources: 
-# - https://www.geeksforgeeks.org/web-information-retrieval-vector-space-model/  
-# - https://www.cl.cam.ac.uk/teaching/1415/InfoRtrv/lecture4.pdf 
-# - https://spotintelligence.com/2023/09/07/vector-space-model/ 
+import math
 
 ''' 
 Objectives: 
 1.) Tokenize query
-2.) Calculate tf-idf scores for each token
-    - Only consider already indexed terms (any term that exists in the index)
-    - for tf use query, for idf use previously calculated idf 
+2.) Calculate tf-idf for query and vectorize it 
 3.) Vectorize documents
-4.) Cosine similarity between query and document vectors to obtain similarity rankings (first row )  
+4.) Cosine similarity between query and document vectors to obtain similarity rankings (first row)  
 5.) Pagination (5 professors per page, non-zero degree of similarity) 
 '''
 
-# X = [0.57735027, 0.57735027, 0.57735027]
-# Y = [1, 1, 0.8]
-# print(cosine_similarity([X,Y]))
+# Document Vectors (variable)
+#
+# Format: 
+#   - key -> faculty URL
+#   - value -> array containing tf-idf vector for every indexed term
+#
+# Example:
+# {
+#   https://www.cpp.edu/faculty/alas : [term1_weight, term2_weight, term3_weight ... termn_weight] ,       
+#   https://www.cpp.edu/faculty/parensburger/index.shtml: [term1_weight, term2_weight, term3_weight ... termn_weight],                                    
+#   ...
+# }
 
-# print(vectorizer.vocabulary_)
-# print(query_vector.shape)
-# print(query_vector.toarray())
-# print(pd.DataFrame(data = query_vector.toarray(),index = ['query'],columns = token_names))
-
-def rank_documents(query,tfidf_index):
+def rank_documents(query,db):
     
-    vectorize_query(query,tfidf_index)
+    # Initialization
+    tfidf_index = list(db.tfidf_index.find())
+    parsed_pages = list(db.parsed_pages.find())
+    document_vectors = {}
+    query_vector = [0] * len(tfidf_index)
     
-    
-def vectorize_query(query,tfidf_index):
+    # Query Tokenization
     vectorizer = CountVectorizer(analyzer='word', stop_words='english')
-    vectorizer.fit(query)
-    tfidf_vectorizer = TfidfVectorizer(analyzer='word',stop_words='english')
-    query_vector = tfidf_vectorizer.fit_transform(query)
-    token_names = tfidf_vectorizer.get_feature_names_out()
+    query_token_counts = vectorizer.fit_transform(query).toarray() # sparse matrix --> array
+    query_tokens = vectorizer.vocabulary_
     
-    # df = pd.DataFrame(data = query_vector.toarray(), index = ['query'], columns = token_names)
-    # for term in df:
-    #     if term in tfidf_index:
-    #         df.drop(term,axis=1)
+    # Initializing document_vectors 
+    for page in parsed_pages:
+        if page["_id"] not in document_vectors:
+            document_vectors[page["_id"]] = [0] * len(tfidf_index)
+    
+    # Add weights from tfidf_index into the document vectors
+    # If the index term is part of the query, calculate its tf-idf score and add it to the query vector
+    for i, term in enumerate(tfidf_index):
         
-            
-    # return query_vector
-
+        if term["_id"] in query_tokens:
+            # tf-idf calculation
+            tf = query_token_counts[0][query_tokens[term["_id"]]] / sum(query_token_counts[0])
+            idf = math.log(len(tfidf_index) / len(term["weights"]))
+            # add to query vector
+            query_vector[i] = tf * idf
+        
+        # Add weights for the term in each document vector from tfidf index
+        for doc, term_weight in term['weights'].items():
+            document_vectors[doc][i] = term_weight
+    
+    # Compiling vectors 
+    all_vectors = []
+    all_vectors.append(query_vector)
+    for vector in document_vectors.values(): 
+        all_vectors.append(vector)
+        
+    # Cosine Similarity 
+    similarity_matrix = cosine_similarity(all_vectors)
+    rankings = similarity_matrix[0][1:len(similarity_matrix[0])] # remove 1st element 
+    print(rankings)
+    
 def connectDatabase():
     DB_NAME = "project"
     DB_HOST = "localhost"
@@ -62,8 +82,7 @@ def connectDatabase():
     except:
         print("Database not connected successfully")
 
-
-test_query = ["The dogs slept behind the churches."]
+# test_query = ["The dogs slept behind the churches."]
+query = [input("Enter a query: ")]
 db = connectDatabase()
-terms = list(db.tfidf_index.find())
-rank_documents(test_query)
+rank_documents(query,db)
